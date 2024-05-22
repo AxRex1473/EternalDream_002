4f3a65de99c2aba8a0e98f3d5be6a33fb67f3fb2
Axel Hernández <89049642+AxRex1473@users.noreply.github.com>
2024-05-22 01:14:54 -0600

    LineRenderer

--- Assets/Script/Controllers/Line.cs
+++ Assets/Script/Controllers/Line.cs
@@ -4,39 +4,48 @@ using UnityEngine;
 
 public class Line : MonoBehaviour
 {
-    private LineRenderer line;
-    private Vector3 previousPosition;
-
-    private float minDistance = 0.1f;
-
-    private void Start()
-    {
-        line = GetComponent<LineRenderer>();
-        line.positionCount = 1;
-        previousPosition = transform.position;
-    }
+    Coroutine drawing;
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0)) 
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            currentPosition.z = 0f;
+            StartLine();
+        }
 
-            if(Vector3.Distance(currentPosition, previousPosition) > minDistance ) 
-            {
-                if(previousPosition == transform.position) 
-                {
-                    line.SetPosition(0, currentPosition);
-                }
-                else
-                {
-                    line.positionCount++;
-                    line.SetPosition(line.positionCount - 1, currentPosition);
-                }
-            }
+        if (Input.GetMouseButtonUp(0))
+        {
+            EndLine();
+        }
+    }
 
-            previousPosition = currentPosition;
+    void StartLine()
+    {
+        if (drawing != null)
+        {
+            StopCoroutine(drawing);
+        }
+        drawing = StartCoroutine(DrawLine());
+    }
+
+    void EndLine()
+    {
+        StopCoroutine(drawing);
+    }
+
+    IEnumerator DrawLine()
+    {
+        GameObject newGameObject = Instantiate(Resources.Load("Line") as GameObject, new Vector3(0, 0, 0), Quaternion.identity);
+        LineRenderer line = newGameObject.GetComponent<LineRenderer>();
+        line.positionCount = 0;
+
+        while (true)
+        {
+            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            position.z = 0;
+            line.positionCount++;
+            line.SetPosition(line.positionCount - 1, position);
+            yield return null;
         }
     }
 }
